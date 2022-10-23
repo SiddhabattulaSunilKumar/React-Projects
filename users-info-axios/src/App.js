8b892af036717d55bf995fcfffbe676ee5124930
SiddhabattulaSunilKumar <siddhabattula.sunil@gmail.com>
2022-10-23 14:28:52 +0100

    userInfo using axios

---
+++ users-info-axios/src/App.js
@@ -0,0 +1,10 @@
+import UsersList from "./Components/UsersList/UsersList";
+function App() {
+  return (
+    <div className="App">
+      <UsersList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ users-info-axios/src/Components/UsersList/UsersList.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import UserInfo from "../UserInfo/UserInfo";
+import axios from "axios";
+
+let UsersList = (props) => {
+  const [usersDetails, setUsersDetails] = useState([]);
+
+  const baseURL = "https://reqres.in/api/users?page=2";
+  const UserDetailsAPI = () => {
+    /*fetch("https://reqres.in/api/users?page=2")
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        const userDet = result.data.map((userData) => {
+          return {
+            id: userData.id,
+            firstName: userData.first_name,
+            lastName: userData.last_name,
+            emailId: userData.email,
+            fullName: userData.first_name + " " + userData.last_name,
+            imageURL: userData.avatar,
+          };
+        });
+        setUsersDetails(userDet);
+      },
+      (error) => {
+        setUsersDetails(error);
+      }
+    );*/
+    axios
+      .get(baseURL)
+      .then((response) => {
+        const userDet = response.data.data.map((userData) => {
+          return {
+            id: userData.id,
+            firstName: userData.first_name,
+            lastName: userData.last_name,
+            emailId: userData.email,
+            fullName: userData.first_name + " " + userData.last_name,
+            imageURL: userData.avatar,
+          };
+        });
+        setUsersDetails(userDet);
+      })
+      .catch((error) => {
+        setUsersDetails(error);
+      });
+  };
+
+  const FetchUserListHandler = (e) => {
+    UserDetailsAPI();
+  };
+
+  return (
+    <Card title="Users Information">
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={FetchUserListHandler}
+        >
+          Fetch Users List
+        </button>
+      </div>
+      <br />
+      <UserInfo onUsersListChaange={usersDetails} />
+    </Card>
+  );
+};
+
+export default UsersList;
